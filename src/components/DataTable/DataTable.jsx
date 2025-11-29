86884bfb8f2511c394d66aad917260f469663335
sieunhan12B <dgbaoo2104@gmail.com>
2025-11-30 00:48:27 +0700

    done category manager

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import { Table } from 'antd';
 import { ConfigProvider } from 'antd';
 import viVN from 'antd/locale/vi_VN'; // Tiếng Việt
-const DataTable = ({ columns, dataSource, totalText,loading }) => {
-
+const DataTable = ({ columns, dataSource, totalText, loading, rowKey = "id" }) => {
     return (
         <div className="p-6">
             <ConfigProvider locale={viVN}>
-            <Table
-                columns={columns}
-                dataSource={dataSource}
-                loading={loading}
-                rowKey="id"
-                pagination={{
-                    pageSize: 10,
-                    showSizeChanger: true,
-                    showTotal: (total, range) => `${range[0]}-${range[1]} của ${total} ${totalText}`,
-                }}
-                scroll={{ x: 1200 }}
-                className="border border-gray-200 rounded-lg"
-            />
-            </ConfigProvider>    
+                <Table
+                    columns={columns}
+                    dataSource={dataSource}
+                    loading={loading}
+                    rowKey={rowKey}
+                    pagination={{
+                        pageSize: 10,
+                        showSizeChanger: true,
+                        showTotal: (total, range) => `${range[0]}-${range[1]} của ${total} ${totalText}`,
+                    }}
+                    scroll={{ x: 1200 }}
+                    className="border border-gray-200 rounded-lg"
+                />
+
+            </ConfigProvider>
         </div>
     )
 }
